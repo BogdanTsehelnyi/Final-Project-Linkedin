92d94322458a03a9ed2602b29bdc5f585dc3ff06
tereshonkov <developer@users-MacBook-Pro.local>
2024-10-01 23:36:48 -0700

    header: decstop vers

--- final-project/src/App.jsx
+++ final-project/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 import AppRoutes from './AppRoutes'
+import Header from './components/Header'
 
 function App() {
   return (
     <>
+    <Header />
     <AppRoutes/>
     </>
   )
--- final-project/src/AppRoutes.jsx
+++ final-project/src/AppRoutes.jsx
@@ -1,7 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Net from "./pages/Net";
+import Jobs from "./pages/Jobs";
+import Messages from "./pages/Messages";
+import Notifications from "./pages/Notifications";
+
 export default function AppRoutes() {
-    return (
-        <>  
-        <h1>Hello World</h1>
-        </>
-    )
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/net" element={<Net />} />
+      <Route path="/jobs" element={<Jobs />} />
+      <Route path="/messages" element={<Messages />} />
+      <Route path="/notifications" element={<Notifications />} />
+    </Routes>
+  );
 }
