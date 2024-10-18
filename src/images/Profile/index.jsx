a15234696587dc34558d047f16d217b8c322cd1b
Bohdan Tsehelnyi <zege0707@gmail.com>
2024-10-18 13:59:44 +0300

    Notifications

--- src/images/Profile/index.jsx
+++ src/images/Profile/index.jsx
@@ -1,5 +1,11 @@
+import { useSelector } from "react-redux";
 import styles from "./Profile.module.scss";
 
 export default function Profile() {
-  return <div className={styles.wrapper}></div>;
+  const profilePicture = useSelector((state) => state.profile.profileData.profilePicture);
+
+
+  return <div className={styles.wrapper}>
+    <img src={profilePicture} alt="Профіль" />
+  </div>;
 }
