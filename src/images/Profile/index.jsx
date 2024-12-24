3de323b2045ad4c347a54c6d6117fa081bccbf30
Bohdan Tsehelnyi <zege0707@gmail.com>
2024-12-24 15:52:27 +0200

    lastVersion

--- src/images/Profile/index.jsx
+++ src/images/Profile/index.jsx
@@ -3,10 +3,13 @@ import styles from "./Profile.module.scss";
 
 export default function Profile() {
   const profileData = useSelector((state) => state.profile.profileData);
+
+  // Перевіряємо шлях до фото профілю
   const profilePicture =
-    profileData?.headerPhotoUrl === "" || profileData?.headerPhotoUrl === undefined
-      ? "/image/profile/photo_ava_default.png"
+    !profileData?.headerPhotoUrl
+      ? `${import.meta.env.BASE_URL}image/profile/photo_ava_default.png`
       : profileData?.headerPhotoUrl;
+
   return (
     <div className={styles.wrapper}>
       <img src={profilePicture} alt="Профіль" />
